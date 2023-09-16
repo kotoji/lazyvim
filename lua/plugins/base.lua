db6e7d4291c3ec4c44f7f7da8fbcde05ec73ada2
kotoji <kotoji@kotojpn.com>
2023-09-16 12:38:57 +0000

    Add metals-nvim (scala) minimal

---
+++ lua/plugins/base.lua
@@ -0,0 +1,12 @@
+return {
+  -- add nord-vim
+  { "kotoji/nord-vim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
