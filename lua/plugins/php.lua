21294ec7d5d4e440dfe3febd02bfdc74e341e94c
kotoji <kotoji@kotojpn.com>
2024-02-29 06:23:32 +0900

    Add php

---
+++ lua/plugins/php.lua
@@ -0,0 +1,91 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          format = {
+            enable = true,
+            brace = "psr12",
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "php" })
+      end
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.blade_formatter,
+          nls.builtins.diagnostics.phpstan.with({
+            extra_args = {
+              "--memory-limit=2G",
+            },
+          }),
+        },
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "blade-formatter",
+        "html-lsp",
+        "intelephense",
+        "php-debug-adapter",
+        "phpstan",
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      ensure_installed = {
+        "php",
+      },
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_setup = true,
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {
+        function(config)
+          -- all sources with no handler get passed here
+
+          -- Keep original functionality
+          require("mason-nvim-dap").default_setup(config)
+        end,
+        php = function(config)
+          config.configurations = {
+            {
+              type = "php",
+              request = "launch",
+              name = "Listen for Xdebug",
+              port = 9003,
+              pathMappings = {
+                ["/var/www/html"] = "${workspaceFolder}",
+              },
+            },
+          }
+          require("mason-nvim-dap").default_setup(config) -- don't forget this!
+        end,
+      },
+    },
+  },
+  {
+    "jwalton512/vim-blade",
+  },
+}
