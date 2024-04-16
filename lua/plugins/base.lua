8387e3944d34f6b0d79b89db650b7f231bce3aa1
kotoji <kotoji@ithnote.blue>
2024-04-16 09:03:55 +0900

    Update neo-tree settings

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -6,7 +6,19 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      --colorscheme = "nord",
+    },
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = false },
+        use_libuv_file_watcher = true,
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
+      },
     },
   },
 }
