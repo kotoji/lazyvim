1a7eaf65e52185ae0650cdd7ae6037751e7d6bde
kotoji <kotoji@ithnote.blue>
2024-11-14 22:44:11 +0900

    Update kotlin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.gleam" },
-    { import = "lazyvim.plugins.extras.lang.kotlin" },
+    -- { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
