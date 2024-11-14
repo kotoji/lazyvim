eb94ae8c8284d08357bc2193d8c1ee1c707fbab3
kotoji <kotoji@ithnote.blue>
2024-11-14 22:49:19 +0900

    Update Lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
