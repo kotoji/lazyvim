ad00259d25e99d90c3f50dc4e14889a41c34cef9
kotoji <kotoji@ithnote.blue>
2025-03-24 07:45:02 +0900

    Add rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.python" },
