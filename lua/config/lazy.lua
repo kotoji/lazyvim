7ab13beaf327aebb452fb20074479ee317cfcec3
kotoji <kotoji@ithnote.blue>
2025-05-12 22:27:50 +0900

    add go & change settings for markdown

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.gleam" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
