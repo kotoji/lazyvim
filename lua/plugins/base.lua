12c7b3b32881696314389ce8f08a5a01ac69ce5c
kotoji <kotoji@ithnote.blue>
2025-08-16 01:52:10 +0900

    update & fix colorschema

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,12 +1,25 @@
 return {
-  -- add nord-vim
-  { "kotoji/nord-vim" },
+  -- nord
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = true
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = false
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       --colorscheme = "nord",
+      colorscheme = "tokyonight-storm",
     },
   },
 
